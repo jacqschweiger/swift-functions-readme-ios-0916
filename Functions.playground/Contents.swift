54fe5744418412de8b9fc30c15e49e504d8fc58d
jacqschweiger <jacqschweiger@gmail.com>
2016-08-30 06:21:24 -0400

    Done

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,14 +1,17 @@
+
 func sayHello() {
     let greeting = "Hello there!"
     print(greeting)
 }
 
 sayHello()
-sayHello()
+// prints "Hello there!"
 
-// Uncomment this line to see the error that is printed in the console.
-// (You can uncomment a line by removing the // at the beginning of the line.)
-//print(greeting)
+sayHello()
+// prints "Hello there!"
+
+
+
 
 
 func sayHelloToGarfield() {
@@ -23,15 +26,15 @@ func sayHelloToScratchy() {
     print("Hello, Scratchy, why do you sleep so much?")
 }
 
-
 func sayHelloToCat(catName: String) {
     print("Hello, \(catName), why do you sleep so much?")
 }
 
 sayHelloToCat("Mittens")
 
-let catFriend = "Socks"
-sayHelloToCat(catFriend)
+sayHelloToCat("Spot")
 
-// Uncomment this line to see the error that is printed in the console.
-//sayHelloToCat()
+let catName = "Bob"
+
+sayHelloToCat(catName)
+
